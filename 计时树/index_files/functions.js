01d2b240c218d0b012abc28babfdb4631b3d61ef
yyh5211314 <3260390633@qq.com>
2025-07-25 21:31:26 +0800

    Add files via upload

---
+++ 计时树/index_files/functions.js
@@ -0,0 +1,62 @@
+/*
+ * http://love.hackerzhou.me
+ */
+
+// variables
+var $win = $(window);
+var clientWidth = $win.width();
+var clientHeight = $win.height();
+
+$(window).resize(function() {
+    var newWidth = $win.width();
+    var newHeight = $win.height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function timeElapse(target) {
+    var now = new Date();
+    var diff = target.getTime() - now.getTime();
+
+    if (diff <= 0) {
+        $("#clock").html("倒计时已结束！");
+        return;
+    }
+
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    var result =
+        " " +
+        "<span class='digit'>" + days + "</span> 天 " +
+        "<span class='digit'>" + hours + "</span> 小时 " +
+        "<span class='digit'>" + minutes + "</span> 分钟 " +
+        "<span class='digit'>" + seconds + "</span> 秒";
+
+    $("#clock").html(result);
+}
